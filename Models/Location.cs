a7bf55a11e54e308c97924e2c26229d9697b76e1
david <david@dev-machine>
2022-01-12 17:33:09 +0000

    Add project files.

---
+++ Models/Location.cs
@@ -0,0 +1,19 @@
+﻿namespace WeatherApp.Models
+{
+    public class LocationArea
+    {
+        public string ID { get; set; }
+        public string LocalizedName { get; set; }
+    }
+
+    public class Location
+    {
+        public int Version { get; set; }
+        public string Key { get; set; }
+        public string Type { get; set; }
+        public int Rank { get; set; }
+        public string LocalizedName { get; set; }
+        public LocationArea Country { get; set; }
+        public LocationArea AdministrativeArea { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Weather App WPF
+
+Using Accuweather and MVVM to create a weather application
